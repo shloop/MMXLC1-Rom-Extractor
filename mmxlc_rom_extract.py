e6fb3353e203fdbc4089828fbbfa90ff01b9e476
shloop <spindlerjonas@gmail.com>
2023-07-21 17:00:54 -0700

    Adds extraction of rev. 1 versions of Mega Man X and Rockman X.

--- mmxlc_rom_extract.py
+++ mmxlc_rom_extract.py
@@ -1,58 +1,102 @@
 #!/usr/bin/env python
 import hashlib
+
 int_sha512 = "905e1d515a77193a23a79d6d92b7e414dca04bb629adc43ca02138a5e26c780baec79b82ec111bb61db4b6f8a07be522ecb90592e864568455c4d12f8cbbfbae"
 jp_sha512 = "07d069b4cc9f20b385cd4500d956c8b8fdeb62d4c4668fc5a302446e718d2338308913e360d887fa25e4766dcd4304025f73360d0e49126f71a0fe865cd3e293"
-with open('RXC1.exe', "rb") as file_to_check:
-    data = file_to_check.read()
-    sha512sum = hashlib.sha512(data).hexdigest()
-if int_sha512 == sha512sum:
-    print("Checksum matches international release, using those offsets for extraction...")
-    with open("RXC1.exe", "rb") as exe:
-        exe.seek(0xB8C4E0,0)
-        rx1 = exe.read(0x180000)
-        exe.seek(0xD8C4E0,0)
-        mx1 = exe.read(0x180000)
-        exe.seek(0xF8C4E0,0)
-        rx2 = exe.read(0x180000)
-        exe.seek(0x110C4E0,0)
-        mx2 = exe.read(0x180000)
-        exe.seek(0x128C4E0,0)
-        rx3 = exe.read(0x200000)
-        exe.seek(0x148C4E0,0)
-        mx3 = exe.read(0x200000)
-elif jp_sha512 == sha512sum:
-    print("Checksum matches Japanese release, using those offsets for extraction...")
-    with open("RXC1.exe", "rb") as exe:
-        exe.seek(0xB8C750,0)
-        rx1 = exe.read(0x180000)
-        exe.seek(0xD8C750,0)
-        mx1 = exe.read(0x180000)
-        exe.seek(0xF8C750,0)
-        rx2 = exe.read(0x180000)
-        exe.seek(0x110C750,0)
-        mx2 = exe.read(0x180000)
-        exe.seek(0x128C750,0)
-        rx3 = exe.read(0x200000)
-        exe.seek(0x148C750,0)
-        mx3 = exe.read(0x200000)
-else:
-    print("Checksum doesn't match any known values, exiting.")
-    exit()
-with open("Rock Man X" + ".sfc", "wb") as rx1out:
-    rx1out.write(rx1)
-    rx1out.close()
-with open("Mega Man X" + ".sfc", "wb") as mx1out:
-    mx1out.write(mx1)
-    mx1out.close()
-with open("Rock Man X2" + ".sfc", "wb") as rx2out:
-    rx2out.write(rx2)
-    rx2out.close()
-with open("Mega Man X2" + ".sfc", "wb") as mx2out:
-    mx2out.write(mx2)
-    mx2out.close()
-with open("Rock Man X3" + ".sfc", "wb") as rx3out:
-    rx3out.write(rx3)
-    rx3out.close()
-with open("Mega Man X3" + ".sfc", "wb") as mx3out:
-    mx3out.write(mx3)
-    mx3out.close()
+
+with open("RXC1.exe", "rb") as exe:
+
+    # Calculate checksum and determine version and offsets
+    
+    sha512sum = hashlib.sha512(exe.read()).hexdigest()
+    if int_sha512 == sha512sum:
+        print("Checksum matches international release, using those offsets for extraction...")
+        
+        rx1_start   = 0xB8C4E0
+        rx1r1_start = 0xD0C4E0
+        mx1_start   = 0xD8C4E0
+        mx1r1_start = 0xF0C4E0
+        rx2_start   = 0xF8C4E0
+        mx2_start  = 0x110C4E0
+        rx3_start  = 0x128C4E0
+        mx3_start  = 0x148C4E0
+
+    elif jp_sha512 == sha512sum:
+        print("Checksum matches Japanese release, using those offsets for extraction...")
+        rx1_start  = 0xB8C750
+        mx1_start  = 0xD8C750
+        rx2_start  = 0xF8C750
+        mx2_start = 0x110C750
+        rx3_start = 0x128C750
+        mx3_start = 0x148C750
+
+    else:
+        print("Checksum doesn't match any known values, exiting.")
+        print(sha512sum)
+        exit()
+
+    # Extract ROM data to files using offsets
+
+    filename = "Rock Man X.sfc"
+    with open(filename, "wb") as out:
+        exe.seek(rx1_start,0)
+        out.write(exe.read(0x180000))
+        out.close()
+        print("Extracted " + filename)
+    
+    filename = "Mega Man X.sfc"
+    with open(filename, "wb") as out:
+        exe.seek(mx1_start,0)
+        out.write(exe.read(0x180000))
+        out.close()
+        print("Extracted " + filename)
+
+    filename = "Rock Man X2.sfc"
+    with open(filename, "wb") as out:
+        exe.seek(rx2_start,0)
+        out.write(exe.read(0x180000))
+        out.close()
+        print("Extracted " + filename)
+
+    filename = "Mega Man X2.sfc"
+    with open(filename, "wb") as out:
+        exe.seek(mx2_start,0)
+        out.write(exe.read(0x180000))
+        out.close()
+        print("Extracted " + filename)
+
+    filename = "Rock Man X3.sfc"
+    with open(filename, "wb") as out:
+        exe.seek(rx3_start,0)
+        out.write(exe.read(0x200000))
+        out.close()
+        print("Extracted " + filename)
+
+    filename = "Mega Man X3.sfc"
+    with open(filename, "wb") as out:
+        exe.seek(mx3_start,0)
+        out.write(exe.read(0x200000))
+        out.close()
+        print("Extracted " + filename)
+
+    if int_sha512 == sha512sum:
+    
+        filename = "Rock Man X r1.sfc"
+        with open(filename, "wb") as out:
+            exe.seek(rx1r1_start,0)
+            out.write(exe.read(0x80000))
+            exe.seek(rx1_start + 0x80000,0)
+            out.write(exe.read(0x100000))
+            out.close()
+            print("Extracted " + filename)
+
+        filename = "Mega Man X r1.sfc"
+        with open(filename, "wb") as out:
+            exe.seek(mx1r1_start,0)
+            out.write(exe.read(0x80000))
+            exe.seek(mx1_start + 0x80000,0)
+            out.write(exe.read(0x100000))
+            out.close()
+            print("Extracted " + filename)
+
+    exe.close()
